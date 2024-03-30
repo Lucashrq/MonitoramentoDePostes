1449457613b494af8e3fa698a961a280e28e24cc
Lucas Henrique <lucaz.hrq@gmail.com>
2024-03-30 16:05:08 -0300

    Atualização nas pastas do Projeto

--- src/br/ifba/edu/monitoramento/objetoMonitorado/PontoIluminacao.java
+++ src/br/ifba/edu/monitoramento/objetoMonitorado/PontoIluminacao.java
@@ -5,13 +5,29 @@ public class PontoIluminacao {
     public boolean luzAcesa;
     public int tempoLuzAcesa; // em minutos
     public int qualidadeDaIluminacao; // 1 = Ótima; 2 = Precisa de atenção; 3 = Sem iluminação.
-    public String qualidadeIluminacao;
+    public int qualidadeIluminacao;
+    public String descricaoQualidade;
 
     public PontoIluminacao(int id, boolean luzAcesa, int tempoLuzAcesa, int qualidadeDaIluminacao) {
         this.id = id;
         this.luzAcesa = luzAcesa;
         this.tempoLuzAcesa = tempoLuzAcesa;
         this.qualidadeDaIluminacao = qualidadeDaIluminacao;
+
+        switch (qualidadeIluminacao) {
+            case 1:
+                descricaoQualidade = "Boa";
+                break;
+            case 2:
+                descricaoQualidade = "Fraca";
+                break;
+            case 3:
+                descricaoQualidade = "Defeituosa";
+                break;
+            default:
+                descricaoQualidade = "Desconhecida"; // Caso o valor não seja 1, 2 ou 3
+        }
+
     }
 
 }
